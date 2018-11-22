a5a78c7c5b2fd08b1d45df237cf434baa14205dc
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2018-11-22 13:46:44 -0800

    jni helpers

--- app/src/main/cpp/jni-helper.cpp
+++ app/src/main/cpp/jni-helper.cpp
@@ -1,4 +1,25 @@
 #include "jni.h"
+#include <cstdint>
+#include <string>
+#include <config.h>
+#include <service.h>
+#include <n2t/n2t.h>
+#include <n2t/n2s.h>
+using namespace std;
+using namespace Net2Tr;
+
+void startTrojan(const string &filename)
+{
+    Config c;
+    c.load(filename);
+    Service(c).run();
+}
+
+void startN2S(int tun_fd, const string &ip_addr, const string &netmask, const string &ip6_addr, uint16_t mtu, const string &socks5_addr, uint16_t socks5_port)
+{
+    N2T n2t(ip_addr, netmask, ip6_addr, mtu);
+    N2S(tun_fd, n2t, socks5_addr, socks5_port).start();
+}
 
 extern "C" {
 }
