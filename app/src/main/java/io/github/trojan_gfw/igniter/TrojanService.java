00b511c50d4d16932a6fd9013e8cbd225221d7cd
Syrone Wong <wong.syrone@gmail.com>
2019-03-24 14:18:07 +0800

    add ipv6 support
    sync vlan4 and vlan6 ip address, netmask and route with shadowsocks-android
    add google ipv6 dns server
    
    shutdown service when onDestroy() is called
    
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- app/src/main/java/io/github/trojan_gfw/igniter/TrojanService.java
+++ app/src/main/java/io/github/trojan_gfw/igniter/TrojanService.java
@@ -5,6 +5,11 @@ import android.net.VpnService;
 import android.os.ParcelFileDescriptor;
 
 public class TrojanService extends VpnService {
+    private static final int VPN_MTU = 1500;
+    private static final String PRIVATE_VLAN4_CLIENT = "172.19.0.1";
+    private static final String PRIVATE_VLAN4_ROUTER = "172.19.0.2";
+    private static final String PRIVATE_VLAN6_CLIENT = "fdfe:dcba:9876::1";
+    private static final String PRIVATE_VLAN6_ROUTER = "fdfe:dcba:9876::2";
     private static TrojanService instance;
     private ParcelFileDescriptor pfd;
 
@@ -17,6 +22,7 @@ public class TrojanService extends VpnService {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        shutdown();
         instance = null;
     }
 
@@ -33,26 +39,34 @@ public class TrojanService extends VpnService {
             e.printStackTrace();
         }
         b.setSession(getString(R.string.app_name));
-        b.setMtu(1500);
-        b.addAddress("10.114.51.4", 31);
+        b.setMtu(VPN_MTU);
+        b.addAddress(PRIVATE_VLAN4_CLIENT, 30);
         b.addRoute("0.0.0.0", 0);
-        b.addDnsServer("1.0.0.1");
+        b.addAddress(PRIVATE_VLAN6_CLIENT, 126);
+        b.addRoute("::", 0);
+        b.addDnsServer("8.8.8.8");
         b.addDnsServer("8.8.4.4");
         b.addDnsServer("1.1.1.1");
-        b.addDnsServer("8.8.8.8");
+        b.addDnsServer("1.0.0.1");
+        b.addDnsServer("2001:4860:4860::8888");
+        b.addDnsServer("2001:4860:4860::8844");
         pfd = b.establish();
         JNIHelper.trojan(getFilesDir() + "/config.json");
-        JNIHelper.n2s(pfd.getFd(), "10.114.51.5", "255.255.255.254", "", 1500, "127.0.0.1", 1080);
+        JNIHelper.n2s(pfd.getFd(), PRIVATE_VLAN4_ROUTER, "255.255.255.0", PRIVATE_VLAN6_ROUTER, VPN_MTU, "127.0.0.1", 1080);
         return START_STICKY;
     }
 
-    public void stop() {
+    private void shutdown() {
         try {
             JNIHelper.stop();
             pfd.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void stop() {
+        shutdown();
         stopSelf();
     }
 }
