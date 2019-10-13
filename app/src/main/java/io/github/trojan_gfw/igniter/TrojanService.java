b1ad7ec82d51ee22b796e21411c0ef09a4ed8391
oasiscifr <oasiscifr@gmail.com>
2019-10-13 03:04:31 +0200

    libs: introduce go-tun2socks [broken]

--- app/src/main/java/io/github/trojan_gfw/igniter/TrojanService.java
+++ app/src/main/java/io/github/trojan_gfw/igniter/TrojanService.java
@@ -3,11 +3,39 @@ package io.github.trojan_gfw.igniter;
 import android.content.Intent;
 import android.net.VpnService;
 import android.os.ParcelFileDescriptor;
+import android.util.Log;
 
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+import clash.Clash;
+import tun2socks.Tun2socks;
+import tun2socks.PacketFlow;
+
+
+class Flow implements PacketFlow {
+    private FileOutputStream flowOutputStream;
+
+    Flow(FileOutputStream stream){
+        flowOutputStream = stream;
+    }
+
+    @Override
+    public void writePacket(byte[] bytes) {
+        try{
+            flowOutputStream.write(bytes);
+        } catch (java.io.IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
 
 public class TrojanService extends VpnService {
     private static final int VPN_MTU = 1500;
@@ -17,6 +45,12 @@ public class TrojanService extends VpnService {
     private static final String PRIVATE_VLAN6_ROUTER = "fdfe:dcba:9876::2";
     private static TrojanService instance;
     private ParcelFileDescriptor pfd;
+    private InputStream inputStream;
+    private FileOutputStream outputStream;
+    private ByteBuffer buffer = ByteBuffer.allocate(1501);
+    private boolean running = false;
+    private Thread packetThread;
+
 
     @Override
     public void onCreate() {
@@ -27,12 +61,33 @@ public class TrojanService extends VpnService {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        shutdown();
         instance = null;
     }
 
     public static TrojanService getInstance() {
         return instance;
+    }
+
+    class PacketThread extends Thread {
+        public void run()
+        {
+            System.out.println("Current thread name: "
+                    + Thread.currentThread().getName());
+            while (running) {
+                try {
+                    int n = inputStream.read(buffer.array());
+                    if (n > 0) {
+                        buffer.limit(n);
+                        Tun2socks.inputPacket(buffer.array());
+                        buffer.clear();
+                    }
+                } catch (IOException e) {
+                    break;
+                }
+            }
+            System.out.println("thread quit");
+            return;
+        }
     }
 
     @Override
@@ -43,7 +98,9 @@ public class TrojanService extends VpnService {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        boolean enable_clash = intent.getBooleanExtra("enable_clash", true);
         boolean enable_ipv6 = false;
+
         File file = new File(getFilesDir(), "config.json");
         if (file.exists()) {
             try {
@@ -74,18 +131,54 @@ public class TrojanService extends VpnService {
             b.addDnsServer("2001:4860:4860::8844");
         }
         pfd = b.establish();
+
+        if ((pfd == null) || !Tun2socks.setNonblock((long) pfd.getFd(), false)) {
+            Log.e("tun2socks", "failed to put tunFd in blocking mode");
+            shutdown();
+            return START_NOT_STICKY;
+        }
+        outputStream = new FileOutputStream(pfd.getFileDescriptor());
+        inputStream = new FileInputStream(pfd.getFileDescriptor());
+        Flow flow = new Flow(outputStream);
+
         JNIHelper.trojan(getFilesDir() + "/config.json");
-        JNIHelper.n2s(pfd.getFd(), PRIVATE_VLAN4_ROUTER, "255.255.255.253", PRIVATE_VLAN6_ROUTER, VPN_MTU, "127.0.0.1", 1080);
+
+        int tun2socksPort;
+        if (enable_clash) {
+            try {
+                Clash.start(getFilesDir().toString());
+                Log.e("Clash", "clash started");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            tun2socksPort = 1080;
+        } else {
+            tun2socksPort = 1081;
+        }
+        Tun2socks.start(flow, "127.0.0.1:" + tun2socksPort, "255.0.128.1", "255.0.143.254");
+        running = true;
+        packetThread = new PacketThread();
+        packetThread.start();
+
         return START_STICKY;
     }
 
     private void shutdown() {
+        running = false;
         try {
+            if (Clash.isRunning()) {
+                Clash.stop();
+                Log.e("Clash", "clash stopped");
+            }
+            Tun2socks.stop();
             JNIHelper.stop();
             pfd.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        pfd = null;
+        inputStream = null;
+        outputStream = null;
     }
 
     public void stop() {
