482604c3509ef629d8555a152b3459cde84fd54f
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2020-01-07 09:05:07 +0800

    Update client ciphers
    Client ciphers are taken from Firefox 71.0.

--- app/src/main/java/io/github/trojan_gfw/igniter/MainActivity.java
+++ app/src/main/java/io/github/trojan_gfw/igniter/MainActivity.java
@@ -57,16 +57,19 @@ public class MainActivity extends AppCompatActivity {
                             .put("cert", getCacheDir() + "/cacert.pem")
                             .put("cipher", "ECDHE-ECDSA-AES128-GCM-SHA256:"
                                     + "ECDHE-RSA-AES128-GCM-SHA256:"
-                                    + "ECDHE-ECDSA-AES256-GCM-SHA384:"
-                                    + "ECDHE-RSA-AES256-GCM-SHA384:"
                                     + "ECDHE-ECDSA-CHACHA20-POLY1305:"
                                     + "ECDHE-RSA-CHACHA20-POLY1305:"
+                                    + "ECDHE-ECDSA-AES256-GCM-SHA384:"
+                                    + "ECDHE-RSA-AES256-GCM-SHA384:"
+                                    + "ECDHE-ECDSA-AES256-SHA:"
+                                    + "ECDHE-ECDSA-AES128-SHA:"
                                     + "ECDHE-RSA-AES128-SHA:"
                                     + "ECDHE-RSA-AES256-SHA:"
-                                    + "RSA-AES128-GCM-SHA256:"
-                                    + "RSA-AES256-GCM-SHA384:"
-                                    + "RSA-AES128-SHA:RSA-AES256-SHA:"
-                                    + "RSA-3DES-EDE-SHA")
+                                    + "DHE-RSA-AES128-SHA:"
+                                    + "DHE-RSA-AES256-SHA:"
+                                    + "AES128-SHA:"
+                                    + "AES256-SHA:"
+                                    + "DES-CBC3-SHA")
                             .put("alpn", new JSONArray().put("h2").put("http/1.1")))
                     .put("enable_ipv6", enableIpv6)
                     .toString();
