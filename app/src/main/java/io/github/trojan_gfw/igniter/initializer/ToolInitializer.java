320303cf02a8c42c11ffa8f236c06f06d720902d
liuyj <c2359438@qq.com>
2020-04-08 16:08:29 +0800

    Write and read exempted app list in file instead of in SharedPreferences. Since ProxyService and MainActivity run in different process, it is better to share the data in file, because SharedPreferences is of bad performance in multiprocess environment. Now that file takes the place of SharedPreferences, CommonSP is deprecated and should be removed.

--- app/src/main/java/io/github/trojan_gfw/igniter/initializer/ToolInitializer.java
+++ app/src/main/java/io/github/trojan_gfw/igniter/initializer/ToolInitializer.java
@@ -3,7 +3,6 @@ package io.github.trojan_gfw.igniter.initializer;
 import android.content.Context;
 
 import io.github.trojan_gfw.igniter.Globals;
-import io.github.trojan_gfw.igniter.common.os.CommonSP;
 import io.github.trojan_gfw.igniter.common.os.MultiProcessSP;
 
 /**
