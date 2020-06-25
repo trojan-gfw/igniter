ceddfdeb48e84a5605fbfd71e01c3a5905f6872b
TchaikovDriver <lauyukit@gmail.com>
2020-06-25 22:06:08 +0800

    Fix the clash of authorities in ContentProvider between release apk and debug apk. Now it is able to install debug version and release version in the same phone again.

--- app/src/main/java/io/github/trojan_gfw/igniter/common/constants/Constants.java
+++ app/src/main/java/io/github/trojan_gfw/igniter/common/constants/Constants.java
@@ -1,7 +1,9 @@
 package io.github.trojan_gfw.igniter.common.constants;
 
+import io.github.trojan_gfw.igniter.BuildConfig;
+
 public abstract class Constants {
-    public static final String PREFERENCE_AUTHORITY = "io.github.trojan_gfw.igniter";
+    public static final String PREFERENCE_AUTHORITY = BuildConfig.APPLICATION_ID;
     public static final String PREFERENCE_PATH = "preferences";
     public static final String PREFERENCE_URI = "content://" + PREFERENCE_AUTHORITY + "/" + PREFERENCE_PATH;
     public static final String PREFERENCE_KEY_ENABLE_CLASH = "enable_clash";
